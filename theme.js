40cc0bd14f3cd362ace1ac86a650efa3f6a8b08e
Jaan Toots <jt551@cam.ac.uk>
2015-03-21 22:09:33 +0000

    Refactored and tidied up juhatus

--- theme.js
+++ theme.js
@@ -1,14 +1,10 @@
-var main = function() {
-    $('#varasemad_toggle').click(function() {
+var main = function () {
+    $('#varasemad_toggle').click(function () {
         $('#varasemad').toggle();
     });
     
-    $('#juh2012_toggle').click(function() {
-        $('#juh2012').toggle();
-    });
-    
-    $('#juh2013_toggle').click(function() {
-        $('#juh2013').toggle();
+    $('#juhhis_toggle').click(function () {
+        $('#juhhis').toggle();
     });
 };
 
