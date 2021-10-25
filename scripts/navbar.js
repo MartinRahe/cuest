2924eb0cdb14117398b2b5cc4a8cd9f04daaceeb
MartinRahe <martin.r.rahe@gmail.com>
2021-10-25 13:28:08 +0100

    test

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,5 +6,6 @@ fetch('navbar.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
-if(self.location.href.split('/').pop() == ""){var id = "index"}else{var id = self.location.href.split('/').pop();}
+if(self.location.href.split('/').pop() == ""){var id = "navbar-brand-main"}else{var id = self.location.href.split('/').pop();}
 alert(id);
+window.onload=function(){setTimeout(()=>{document.getElementById(id).className="active";},50)};
