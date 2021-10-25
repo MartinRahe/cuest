4e41ac3f4e62232d02a5f721f11f30a57b6750b3
MartinRahe <martin.r.rahe@gmail.com>
2021-10-25 13:51:22 +0100

    navbar script update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,8 +6,5 @@ fetch('navbar.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
-if(self.location.href.split('/').pop() == "")
-{var id = "navbar-brand-main";
-window.onload=function(){setTimeout(()=>{document.getElementById(id).className+=" active";},50)};}
-else{var id = self.location.href.split('/').pop();
-window.onload=function(){setTimeout(()=>{document.getElementById(id).className="active";},50)};}
+if(self.location.href.split('/').pop() == ""){var id = "index";}else{var id = self.location.href.split('/').pop();}
+window.onload=function(){setTimeout(()=>{document.getElementById(id).className+=" active";},50)};
