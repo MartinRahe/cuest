549b8f7bff5ecd70490020c51a996b325ff8e37f
MartinRahe <martin.r.rahe@gmail.com>
2021-11-09 17:27:11 +0000

    dark mode button test

--- scripts/toggleTheme.js
+++ scripts/toggleTheme.js
@@ -1,1 +1,7 @@
-const htmlEl = document.getElementsByTagName('html')[0];
+function toggleTheme() {
+	const htmlEl = document.getElementsByTagName('html')[0];
+	const toggleTheme = (theme) => {
+		htmlEl.dataset.theme = theme;
+	}
+	alert("test");
+}
