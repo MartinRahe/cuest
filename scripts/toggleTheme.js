6650cf8bd20d2727499c433017d7370aad77e8b6
MartinRahe <martin.r.rahe@gmail.com>
2021-11-09 20:02:58 +0000

    dark mode memory

--- scripts/toggleTheme.js
+++ scripts/toggleTheme.js
@@ -1,5 +1,9 @@
 const htmlEl = document.getElementsByTagName('html')[0];
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+    htmlEl.dataset.theme = currentTheme;
+}
 function toggleTheme(theme) {
 	htmlEl.setAttribute('data-theme',theme);
+    localStorage.setItem('theme', theme);
 }
