2cf128c628f5122da1f2952caeac84ebfa662c62
MartinRahe <martin.r.rahe@gmail.com>
2021-11-10 02:04:07 +0000

    switch test

--- scripts/toggleTheme.js
+++ scripts/toggleTheme.js
@@ -1,5 +1,5 @@
 const htmlEl = document.getElementsByTagName('html')[0];
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+var currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 if (currentTheme) {
     htmlEl.dataset.theme = currentTheme;
 }
